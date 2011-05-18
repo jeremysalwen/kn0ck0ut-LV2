941809da610ac8260cf6ea169d08ab1d3e035889
Jeremy Salwen <jeremysalwen@gmail.com>
2011-05-18 02:37:47 -0400

    Removed fft function.  Will replace with fftw.

--- kn0ck0ut6.cpp
+++ kn0ck0ut6.cpp
@@ -151,15 +151,6 @@ float AKnockout::getParameter (long index)
 	}
 	return v;
 }
-//-----------------------------------------------------------------------------------------
-
-void AKnockout::getParameterName(long index, char *label)
-{
-	switch (index)
-	{
-
-	}
-}
 
 
 //-----------------------------------------------------------------------------------------
@@ -355,63 +346,4 @@ void AKnockout::makelookup(int fftFrameSize)
 
 for (int k=0; k<fftFrameSize; k++) window[k] = -.5*cos(2*PI*(double)k/(double)fftFrameSize)+.5;
 
-}
-// -----------------------------------------------------------------------------------------------------------------
-
-
-inline void AKnockout::smsFft(float *fftBuffer, long fftFrameSize, long sign)
-/* 
-	FFT routine, (C)1996 S.M.Sprenger. Sign = -1 is FFT, 1 is iFFT (inverse)
-	Fills fftBuffer[0...2*fftFrameSize-1] with the Fourier transform of the
-	time domain data in fftBuffer[0...2*fftFrameSize-1]. The FFT array takes
-	and returns the cosine and sine parts in an interleaved manner, ie.
-	fftBuffer[0] = cosPart[0], fftBuffer[1] = sinPart[0], asf. fftFrameSize
-	must be a power of 2. It expects a complex input signal (see footnote 2),
-	ie. when working with 'common' audio signals our input signal has to be
-	passed as {in[0],0.,in[1],0.,in[2],0.,...} asf. In that case, the transform
-	of the frequencies of interest is in fftBuffer[0...fftFrameSize].
-*/
-{
-	float wr, wi, arg, *p1, *p2, temp;
-	float tr, ti, ur, ui, *p1r, *p1i, *p2r, *p2i;
-	long i, bitm, j, le, le2, k;
-	long loopCondition = (long) (log(fftFrameSize)/log(2.)); 		// only a 
-	long twiceFftFS = fftFrameSize*2;								// few tiny
-
-	for (i = 2; i < twiceFftFS-2; i += 2) {
-		for (bitm = 2, j = 0; bitm < twiceFftFS; bitm <<= 1) {
-			if (i & bitm) j++;
-			j <<= 1;
-		}
-		if (i < j) {
-			p1 = fftBuffer+i; p2 = fftBuffer+j;
-			temp = *p1; *(p1++) = *p2;
-			*(p2++) = temp; temp = *p1;
-			*p1 = *p2; *p2 = temp;
-		}
-	}
-	for (k = 0, le = 2; k < loopCondition; k++) { 					// changes to
-		le <<= 1;
-		le2 = le>>1;
-		ur = 1.0;
-		ui = 0.0;
-		arg = PI / (le2>>1);
-		wr = myQT.QuickCos(arg); 									// sm sprenger's
-		wi = sign*myQT.QuickSin(arg);								// fft code
-		for (j = 0; j < le2; j += 2) {
-			p1r = fftBuffer+j; p1i = p1r+1;
-			p2r = p1r+le2; p2i = p2r+1;
-			for (i = j; i < twiceFftFS; i += le) {
-				tr = *p2r * ur - *p2i * ui;
-				ti = *p2r * ui + *p2i * ur;
-				*p2r = *p1r - tr; *p2i = *p1i - ti;
-				*p1r += tr; *p1i += ti;
-				p1r += le; p1i += le;
-				p2r += le; p2i += le;
-			}
-			tr = ur*wr - ui*wi;
-			ui = ur*wi + ui*wr;
-			ur = tr;
-		}
-	}
 }
